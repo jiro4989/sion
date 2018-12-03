1090a06b48286b96c2b29c17df0f3ff5dc14e435
jiro4989 <jiroron666@gmail.com>
2018-12-04 00:39:14 +0900

    [update]イメージを変更した

--- testdata/example.sh
+++ testdata/example.sh
@@ -5,25 +5,25 @@ set -eu
 sion help
 sion config init
 
-# 連続してコマンドを投げたい場合
-sion session open &
+# 対象サーバとコネクションを貼る(サーバ)
+# 貼ってる間はロックファイルを生成する
+sion connection open web &
 
-sion command localhost ls
-sion command localhost git clone https://github.com/jiro4989/arth --before 'which arth'
-sion command server1 cp hoge.conf /tmp/hoge.conf -o root -g root
-sion command server1 mkdir /var/log/test -o root -g develop
-sion command server1 groupadd developers
-sion command server1 adduser developer1 -g developers
-sion command server1 replace hoge.conf 'hoge' '#hoge'
-sion command server1 rm hoge.conf
-sion command server1 yum httpd
-sion command web yum jboss
-sion command web yum netutil
-sion command db yum mysqlclients
-sion command db yum go
-sion command db yum oracle
+# 対象サーバにコマンドをなげる(クライアント)
+# クライアント・サーバ方式でコネクションを張りっぱなしにしてコマンドを投げ続ける
+sion command ls
+sion command git clone https://github.com/jiro4989/arth --before 'which arth'
+sion command cp hoge.conf /tmp/hoge.conf -o root -g root
+sion command mkdir /var/log/test -o root -g develop
+sion command groupadd developers
+sion command adduser developer1 -g developers
+sion command replace hoge.conf 'hoge' '#hoge'
+sion command rm hoge.conf
+sion command yum httpd
 
-sion session run
+# サーバを停止、ロックファイルを削除
+sion connection close web
 
-# 都度SSHしてコマンドを投げる場合はsession openしない
-sion command db yum oracle
+# いちいちコネクションを貼るのがめんどくさくて、都度コネクションを貼るので十分な
+# 場合はこっち
+sion command standalone db yum oracle
